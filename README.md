4ae9ef13d089408411285d13370f89a744c4c8df
luca28 <luca.tedeschini00@gmail.com>
2024-12-20 23:57:37 +0100

    add: Second video content

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# openGL_project
- Me trying to understand openGL
+# OpenGL_project
+I wanted to learn OpenGL just because, too much python recently, back to the origin.
+Right now this is still a big WIP. Maybe in the future there will be something to see.
+
+## Resources
+I'm mainly following [this playlist](https://www.youtube.com/watch?v=_POT8K638VY&list=PLysLvOneEETPlOI_PI4mJnocqIpr2cSHS&index=2) 
+and [learnopengl](https://learnopengl.com/) to learn the basics. I've also left comments in the code as notes, so they
+can be useful to someone.
+
+## SW requirements
+I'm working on Ubuntu 22.04
---
+++ src/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main(){
+    glfwInit();
+    // OpenGL version 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // If on MacOS...
+# ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Hello World!", NULL, NULL);
+    if (window == NULL) {
+        // If unable to create the window...
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Where to locate the window? how big?
+    glViewport(0, 0, 800, 600);
+
+    // If I resize the window, add a callback
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while (!glfwWindowShouldClose(window)) {
+        // Process inputs
+        processInput(window);
+
+        // render some colors
+        glClearColor(0.2f, 0.3f, 0.5f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // The gpu generate the next frame at the same time we are showing the current frame.
+        // So everytime I generate a new frame, swap so it can be shown
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+        // A single buffer would cause flickering, because you are showing a frame that is being generated.
+        // Using a double buffer like so, we avoid this issue because the generation can finish properly before displaying
+        // the new frame
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    // Everytime it's resized, set the viewport
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
